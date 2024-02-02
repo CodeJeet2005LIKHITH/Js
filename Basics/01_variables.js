6d6cb92dcd6ad6ab5391870687a90518a645a68d
CV.LIKHITH <143379120+CodeJeet2005LIKHITH@users.noreply.github.com>
2024-02-02 04:01:54 +0000

    committed

---
+++ Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Likhithcvsrl@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nellore"//Not a Good Way
+let accountState
+// AccountState will give output as "Undefined" Because it is Undefined
+
+// accountId = 2;
+// console.log(accountId) Not allowed to change Const Variables
+// JavaScript has problems has problems with Scoping "var has Scope Problem"
+// "let" Dont have any Scope Problem
+
+accountEmail = "hc#Hc"
+accountPassword = "iojc3948"
+accountCity = "Kathmandu"
+console.table([accountEmail,accountPassword,accountCity,accountState])
